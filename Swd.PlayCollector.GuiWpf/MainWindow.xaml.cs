d839a36b0b3df7e743a46ba86eff8f0de5c7956b
Butterweck Marcel <swdeveloper@butterweck.at>
2022-02-24 20:24:39 +0100

    v0.2022.02.24c Korrektur WPF Gui Framework

---
+++ Swd.PlayCollector.GuiWpf/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Swd.PlayCollector.Business;
+using Swd.PlayCollectory.Business.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Swd.PlayCollector.GuiWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+            CollectionItemManager manager = new CollectionItemManager();
+            List<CollectionItem> itemList = manager.GetAll().ToList();
+            this.lstItem.ItemsSource = itemList;
+
+        }
+    }
+}
