6f8f483bfd8bad535b69c869bf760ca6038a3da0
Butterweck Marcel <swdeveloper@butterweck.at>
2022-03-08 19:44:13 +0100

    v0.2022.03.08b Loggin Funktionalität

--- Swd.PlayCollector.GuiWpf/App.xaml.cs
+++ Swd.PlayCollector.GuiWpf/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using log4net;
+using System.Reflection;
 
 namespace Swd.PlayCollector.GuiWpf
 {
@@ -13,6 +15,21 @@ namespace Swd.PlayCollector.GuiWpf
     /// </summary>
     public partial class App : Application
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(App));
+
+
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            Log.Debug(string.Format("{0} Application starting", MethodBase.GetCurrentMethod().Name));
+            
+
+            MainWindow w = new MainWindow();
+            w.Show();
+
+
+        }
 
 
     }
--- Swd.PlayCollector.GuiWpf/MainWindow.xaml.cs
+++ Swd.PlayCollector.GuiWpf/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Swd.PlayCollectory.Business.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,11 +29,16 @@ namespace Swd.PlayCollector.GuiWpf
         }
 
 
-        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             LoadItems();
         }
 
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            LoadItems();
+        }
 
 
         private void lstItem_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -43,13 +49,50 @@ namespace Swd.PlayCollector.GuiWpf
                 txbNumber.Text = selectedItem.Number; 
                 txbName.Text = selectedItem.Name;   
             }
-
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
         {
+            ClearFields();
+        }
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionItemManager manager = new CollectionItemManager();
+            CollectionItem item = lstItem.SelectedItem as CollectionItem;
+            if (item != null)
+            {
+                item.Number = txbNumber.Text;
+                item.Name = txbName.Text;
+                manager.Update(item);
+            }
+            else
+            {
+                item = new CollectionItem();
+                item.Number = txbNumber.Text;
+                item.Name = txbName.Text;
+                item.Created = DateTime.Now;
+                item.CreatedBy = WindowsIdentity.GetCurrent().Name;
+                manager.Insert(item);   
+            }
             LoadItems();
         }
+
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionItemManager manager = new CollectionItemManager();
+            CollectionItem item = lstItem.SelectedItem as CollectionItem;
+            if (item != null)
+            {
+                manager.Delete(item.Id);
+                LoadItems();
+                ClearFields();
+            }
+        }
+
 
 
 
@@ -58,13 +101,17 @@ namespace Swd.PlayCollector.GuiWpf
             CollectionItemManager manager = new CollectionItemManager();
             List<CollectionItem> itemList = manager.GetAll().ToList();
             this.lstItem.ItemsSource = itemList;
-
         }
 
-        private void btnNew_Click(object sender, RoutedEventArgs e)
+
+        private void ClearFields()
         {
+            lstItem.SelectedItem = null;
             txbName.Text = String.Empty;
-            txbNumber.Text= String.Empty;
+            txbNumber.Text = String.Empty;
+
         }
+
+
     }
 }
